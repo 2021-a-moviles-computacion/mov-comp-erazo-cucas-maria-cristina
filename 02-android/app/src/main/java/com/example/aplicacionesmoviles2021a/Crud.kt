2c2a9df37fb3816a7b6f8b923abaa24ea3a6cba0
María Cristina <maria.erazo@epn.edu.ec>
2021-07-06 17:51:22 -0500

    Crud sin funcionar

--- 02-android/app/src/main/java/com/example/aplicacionesmoviles2021a/Crud.kt
+++ 02-android/app/src/main/java/com/example/aplicacionesmoviles2021a/Crud.kt
@@ -4,27 +4,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class Crud : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_crud)
 
+        val txtNombre= findViewById<EditText>(R.id.txtNombre)
+        val txtId= findViewById<EditText>(R.id.txtId)
+        val txtDescripcion= findViewById<EditText>(R.id.txtDescripcion)
 
 
         val botonConsultar = findViewById<Button>(R.id.btn_consultar)
         botonConsultar.setOnClickListener{
-            val usuarioEncontrado = EBaseDeDatos?.TablaUsuario?.consultarUsuarioPorId(1)
+            val usuarioEncontrado = EBaseDeDatos?.TablaUsuario?.consultarUsuarioPorId(5)
             Log.i("bdd", "id:${usuarioEncontrado?.id} Nombre:${usuarioEncontrado?.nombre}"+
                     "Descripcion: ${usuarioEncontrado?.descripcion}")
         }
 
         val botonCrear = findViewById<Button>(R.id.btn_guardar)
         botonCrear.setOnClickListener{
-            val resultado = EBaseDeDatos.TablaUsuario?.crearUsuarioFormulario("MarC","HOLA QUE TAL")
+            val resultado = EBaseDeDatos.TablaUsuario?.crearUsuarioFormulario("${txtNombre.text.toString()}","${txtDescripcion.text.toString()}")
             if(resultado != null){
                 if(resultado){
                     Log.i("bdd","El registro se ha creado")
+
 
                 }else{
                     Log.i("bdd","error")
@@ -35,10 +41,25 @@ class Crud : AppCompatActivity() {
 
         val botonActualizar= findViewById<Button>(R.id.btn_actualizar)
         botonActualizar.setOnClickListener{
-            val resultado = EBaseDeDatos.TablaUsuario?.actualizarUsuarioFormulario("Cristina", "probando",1)
+            val resultado = EBaseDeDatos.TablaUsuario?.actualizarUsuarioFormulario("${txtNombre.text.toString()}", "${txtDescripcion.text.toString()}",1)
             if(resultado != null){
                 if(resultado){
                     Log.i("bdd","Actualizacion exitosa")
+
+
+                }else{
+                    Log.i("bdd","error")
+                }
+            }
+
+        }
+
+        val botonEliminar = findViewById<Button>(R.id.btn_eliminar)
+        botonEliminar.setOnClickListener{
+            val resultado= EBaseDeDatos.TablaUsuario?.eliminarUsuarioFormulari(1)
+            if(resultado != null){
+                if(resultado){
+                    Log.i("bdd","Eliminacion exitosa")
 
                 }else{
                     Log.i("bdd","error")
@@ -46,14 +67,11 @@ class Crud : AppCompatActivity() {
             }
         }
 
-        val botonEliminar = findViewById<Button>(R.id.btn_eliminar)
-        botonEliminar.setOnClickListener{
-            val resultado= EBaseDeDatos.TablaUsuario?.eliminarUsuarioFormulari(1)
+        fun limpiar(){
+            txtNombre.setText("")
+            txtDescripcion.setText("")
         }
-
-
     }
-
 
 
     
